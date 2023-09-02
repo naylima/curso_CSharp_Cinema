d57a918b882a72a01c3e85e17d167bcce0f0a9c8
Nayara Lima <nayaralima@poli.ufrj.br>
2023-09-01 22:21:46 -0300

    Update Program.cs

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -1,12 +1,4 @@
 ﻿namespace Cinema;
-/*
-    * Crie um projeto Cinema
-    *  Crie uma classe Filme com Titulo, Ano, Duracao
-    *  Crie uma classe Sala com quantidadeCadeiras
-    *  Crie uma classe Sessao com private Filme, private Sala e private DataHora
-    *  No construtor da Sessao receba a data e a sala // Injeção de dependencia via construtor
-    *  Crie um método para receber o Filme
-*/
 internal class Program
 {
     private static void Main(string[] args)
